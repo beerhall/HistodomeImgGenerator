5222ce96870203ea6d57f543b6bbdacd56665f36
beerhall <wzh970719@gmail.com>
2018-03-13 19:56:42 +0800

    ExcelReader

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,15 @@
  */
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import util.ExcelReader;
 
 /**
  * 主类
@@ -19,11 +26,26 @@ public class Main {
    */
   public static void main(String[] args) {
     Logger log = LogManager.getLogger(Main.class.getName());
-    log.info("This is an info log");
-    log.debug("This is an debug log");
-    log.warn("This is an warn log");
-    log.error("This is an error log");
-
+    try {
+      HashMap<String, ArrayList<String>> data =
+          ExcelReader.readExcel(new File("input\\test\\timeline.xlsx"));
+      for (String key : data.keySet()) {
+        System.out.print(key + "\t");
+        for (String value : data.get(key)) {
+          System.out.print(value + "\t");
+        }
+        System.out.println();
+      }
+    } catch (IOException e) {
+      log.error(e);
+      e.printStackTrace();
+    } catch (Exception e) {
+      StringWriter stringWriter = new StringWriter();
+      PrintWriter  printWriter  = new PrintWriter(stringWriter);
+      e.printStackTrace(printWriter);
+      log.error(stringWriter.toString());
+      printWriter.close();
+      throw e;
+    }
   }
-
 }
--- src/util/ExcelReader.java
+++ src/util/ExcelReader.java
@@ -4,10 +4,20 @@
 package util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * Excel读取器
@@ -21,12 +31,45 @@ public class ExcelReader {
    * 
    * @param file 文件
    * @return 第一列为键，每行剩下内容组成的ArrayList
+   * @throws IOException
    */
-  static public HashMap<String, ArrayList<String>> readExcel(File file) {
+  static public HashMap<String, ArrayList<String>> readExcel(File file) throws IOException {
     Logger log = LogManager.getLogger(ExcelReader.class.getName());
     log.debug("读取Excel文件：" + file.getName());
     HashMap<String, ArrayList<String>> data = new HashMap<String, ArrayList<String>>();
-    return null;
+    try {
+      FileInputStream fis = new FileInputStream(file);
+      Workbook wb = new XSSFWorkbook(fis);
+      Sheet sheet = wb.getSheetAt(0);
+      for (Iterator<Row> i_row = sheet.rowIterator(); i_row.hasNext();) {
+        Row row = i_row.next();
+        String key = null;
+        ArrayList<String> rowData = new ArrayList<String>();
+        for (Iterator<Cell> i_cell = row.cellIterator(); i_cell.hasNext();) {
+          Cell cell = i_cell.next();
+          cell.setCellType(CellType.STRING);
+          String value = cell.getStringCellValue();
+          if (key == null) {
+            key = value;
+          } else {
+            rowData.add(value);
+          }
+        }
+        data.put(key, rowData);
+      }
+      wb.close();
+    } catch (FileNotFoundException e) {
+      log.error("FileNotFoundException - 未找到Excel文件：" + file.getName());
+      log.error(e.getLocalizedMessage());
+      e.printStackTrace();
+      throw e;
+    } catch (IOException e) {
+      log.error("IOException - 输入/输出错误");
+      log.error(e.getLocalizedMessage());
+      e.printStackTrace();
+      throw e;
+    }
+    return data;
   }
 
 }
